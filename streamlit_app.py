01c33babdccfa3bd9c8d80090f3e4f4fc6d14b9c
pawelswietach <pawel.swietach@dpag.ox.ac.uk>
2026-04-04 22:19:11 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,19 +63,19 @@ if st.button("Solve"):
 
     fig, axs = plt.subplots(2,4,figsize=(18,10))
 
-    axs[0,0].plot(depth,out["O2_mM"]); axs[0,0].set_title("O2")
-    axs[0,1].plot(depth,out["Glu_mM"]); axs[0,1].set_title("Glucose")
-    axs[0,2].plot(depth,out["CO2_mM"]); axs[0,2].set_title("CO2")
-    axs[0,3].plot(depth,out["HLac_mM"]); axs[0,3].set_title("Lactic acid")
+    axs[0,0].plot(depth,out["O2_mM"]); axs[0,0].set_title("O2 (mM)")
+    axs[0,1].plot(depth,out["Glu_mM"]); axs[0,1].set_title("Glucose (mM)")
+    axs[0,2].plot(depth,out["CO2_mM"]); axs[0,2].set_title("CO2 (mM)")
+    axs[0,3].plot(depth,out["HLac_mM"]); axs[0,3].set_title("Lactic acid (mM)")
 
     axs[1,0].plot(depth,out["HCO3e_mM"],'r'); axs[1,0].plot(depth,out["HCO3i_mM"],'b')
-    axs[1,0].set_title("Bicarbonate")
+    axs[1,0].set_title("Bicarbonate (mM)")
 
     axs[1,1].plot(depth,out["pHe"],'r'); axs[1,1].plot(depth,out["pHi"],'b')
     axs[1,1].set_title("pH")
 
     axs[1,2].plot(depth,out["Lace_mM"],'r'); axs[1,2].plot(depth,out["Laci_mM"],'b')
-    axs[1,2].set_title("Lactate")
+    axs[1,2].set_title("Lactate (mM)")
 
     axs[1,3].plot(out["O2_mM"],out["pHe"],'r'); axs[1,3].plot(out["O2_mM"],out["pHi"],'b')
     axs[1,3].set_title("pH vs O2")
