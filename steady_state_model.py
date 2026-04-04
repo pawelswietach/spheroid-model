ec2a7a3116a0d6545cc2d04a05ce0af2f842e1dd
pawelswietach <pawel.swietach@dpag.ox.ac.uk>
2026-04-04 21:39:50 +0100

    Add files via upload

--- steady_state_model.py
+++ steady_state_model.py
@@ -1,48 +1,23 @@
 
 import numpy as np
-from scipy.optimize import least_squares
+from scipy.integrate import solve_ivp
 
 
 def _parse_nhe(nhe):
     if isinstance(nhe, str):
-        key = nhe.strip().lower()
-        if key in {"yes","y","true","1","on"}:
-            return 1.0
-        if key in {"no","n","false","0","off"}:
-            return 0.0
-        raise ValueError(f"Unrecognized NHE value: {nhe}")
+        return 1.0 if nhe.lower() in ["yes","y","true","1"] else 0.0
     return float(nhe)
 
 
-def jac_sparsity(n_points, n_vars=10):
-    N = n_points * n_vars
-    S = np.zeros((N, N), dtype=bool)
+class Model:
 
-    for i in range(n_points):
-        for j in range(n_vars):
-            idx = i*n_vars + j
-            S[idx, idx] = True
-
-            if i > 0:
-                S[idx, (i-1)*n_vars:(i)*n_vars] = True
-            if i < n_points-1:
-                S[idx, (i+1)*n_vars:(i+2)*n_vars] = True
-
-    return S
-
-
-class SteadyStateModel:
-
-    def __init__(self, R, RR, GR, ve, startO2, startCO2, startHCO3, startGlucose, NHE, n_points=60):
+    def __init__(self, R, RR, GR, ve, startO2, startCO2, startHCO3, startGlucose, NHE, n_points=80):
 
         self.R = R
-        self.RR = RR
-        self.GR = GR
         self.ve = ve
         self.vi = 1 - ve
         self.NHE = _parse_nhe(NHE)
 
-        self.n = n_points
         self.x = np.linspace(0, R, n_points)
         self.dx = self.x[1] - self.x[0]
 
@@ -51,13 +26,12 @@ class SteadyStateModel:
 
         D_free = np.array([2600,2100,1300,10,1000,1000,960,0,0,0])
         self.D = np.concatenate([D_free[:2], D_free[2:] * ve])
-        self.D[self.D == 0] = 1e-12
 
         self.CA = 100
         self.kh = 0.14
-        self.kr = self.kh / (10**-6.1)
+        self.kr = self.kh/(10**-6.1)
         self.kf = 1e6
-        self.kb = self.kf / (10**-3.9)
+        self.kb = self.kf/(10**-3.9)
 
         self.Km = 1e-6
         self.Kg = 1e-3
@@ -65,8 +39,8 @@ class SteadyStateModel:
         self.Href = 10**-7.2
         self.Knhe = 10**-6.5
 
-        self.JR_const = (RR/1000)/60
-        self.JG_const = (GR/1000)/60
+        self.JR = (RR/1000)/60
+        self.JG = (GR/1000)/60
 
         startO2 /= 1000
         startCO2 /= 1000
@@ -79,24 +53,18 @@ class SteadyStateModel:
             startO2,startCO2,startHCO3,He_blood,0,0,startGlucose
         ])
 
-        self.c_vec = np.array([1,1,ve,ve,ve,1,1,self.vi,self.vi,self.vi])
+        self.c_vec = np.array([1,1,self.ve,self.ve,self.ve,1,1,self.vi,self.vi,self.vi])
 
-    def initial_guess(self):
-        HCO3i = self.c_blood[1] * 10**(7.2 - 6.1)
+    def initial(self):
+        HCO3i = self.c_blood[1]*10**(7.2-6.1)
         base = np.concatenate([self.c_blood,[HCO3i,10**-7.2,0]])
-
-        U = np.tile(base, (self.n,1))
-
-        # break symmetry with gradient
-        for i in range(self.n):
-            r = self.x[i] / self.R
-            U[i,:] *= (1 - 0.3*r)
-
+        U = np.tile(base,(len(self.x),1))
         return U.ravel()
 
-    def residual(self, y):
+    def rhs(self,t,y):
 
-        U = y.reshape(self.n, 10)
+        n = len(self.x)
+        U = y.reshape(n,10)
 
         O2,CO2,HCO3e,He,Lace,HLac,Glu,HCO3i,Hi,Laci = U.T
 
@@ -105,8 +73,8 @@ class SteadyStateModel:
         r_HLace = self.kb*Lace*He - self.kf*HLac
         r_HLaci = self.kb*Laci*Hi - self.kf*HLac
 
-        JR = self.JR_const * Glu/(Glu+self.Kg) * O2/(O2+self.Km)
-        JG = self.JG_const * Glu/(Glu+self.Kg)
+        JR = self.JR * Glu/(Glu+self.Kg) * O2/(O2+self.Km)
+        JG = self.JG * Glu/(Glu+self.Kg)
 
         Jnhe = (self.NHE/1000/60)*(
             (Hi**2/(Hi**2+self.Knhe**2)) -
@@ -136,31 +104,24 @@ class SteadyStateModel:
         dU[0,:] = self.D * (3*(U[1,:]-U[0,:]) / self.dx**2)
         dU[-1,:] = 0
 
-        F = (dU + s) / self.c_vec
+        rhs = (dU + s) / self.c_vec
+        rhs[-1,:7] = 0
 
-        # boundary conditions only via residual
-        F[0,:] = U[1,:] - U[0,:]
-        F[-1,:7] = U[-1,:7] - self.c_blood
-        F[-1,7:] = U[-1,7:] - U[-2,7:]
-
-        return F.ravel()
+        return rhs.ravel()
 
     def solve(self):
 
-        S = jac_sparsity(self.n)
-
-        sol = least_squares(
-            self.residual,
-            self.initial_guess(),
-            method='trf',
-            jac_sparsity=S,
-            xtol=1e-6,
-            ftol=1e-6,
-            gtol=1e-6,
-            max_nfev=200
+        sol = solve_ivp(
+            self.rhs,
+            (0,1500),
+            self.initial(),
+            method="BDF",
+            rtol=1e-4,
+            atol=1e-7,
+            max_step=200
         )
 
-        U = sol.x.reshape(self.n,10)
+        U = sol.y[:,-1].reshape(len(self.x),10)
 
         return {
             "x_um": self.x,
@@ -178,5 +139,5 @@ class SteadyStateModel:
         }
 
 
-def diffusion_steady_state(**kwargs):
-    return SteadyStateModel(**kwargs).solve()
+def diffusion_solver(**kwargs):
+    return Model(**kwargs).solve()
